d0bb2d9ba6579f552eaabb56fb288336bcffadae
saburi004 <saburinikam@gmail.com>
2025-06-10 10:18:37 +0530

    saburi

--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
 import Image from "next/image";
 import Hero from "@/components/home/Hero";
+import Auth from "@/components/Auth";
+import Shop from "@/components/Shop";
+
 export default function Home() {
   return (
     <>
-    <Hero/>
+    {/* <Hero/> */}
+    <Shop/>
+
     </>
   );
 }
