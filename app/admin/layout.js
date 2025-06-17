163053d4c909b6b57993462fe48df325849dce47
saburi004 <saburinikam@gmail.com>
2025-06-17 15:46:27 +0530

    admin login done

--- app/admin/layout.js
+++ app/admin/layout.js
@@ -3,7 +3,6 @@
 import AdminLayout from '../../components/admin/Admin';
 
 export default function RootLayout({ children }) {
-   if (!isAuthenticated) {
-  }
+  
   return <AdminLayout>{children}</AdminLayout>;
 }
